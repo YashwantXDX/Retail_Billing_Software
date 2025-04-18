17a4c9ac69245025b76df2d467fca118a15cc96d
YashwantXDX <yashwant.choure73546@gmail.com>
2025-04-18 11:42:57 +0530

    Manage Items Component Done

--- client/src/pages/ManageItems/ManageItems.jsx
+++ client/src/pages/ManageItems/ManageItems.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './ManageItems.css'
+import ItemForm from '../../components/ItemForm/ItemForm';
+import ItemList from '../../components/ItemList/ItemList';
 
 const ManageItems = () => {
   return (
     <div className="items-container text-light">
       <div className="left-column">
-        Items Form
+        <ItemForm />
       </div>
       <div className="right-column">
-        list of Items
+        <ItemList />
       </div>
     </div>
   )
