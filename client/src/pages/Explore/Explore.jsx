50ecff99916283d3f519dc8f962bd7e6ceb4bbf7
YashwantXDX <yashwant.choure73546@gmail.com>
2025-04-18 13:23:02 +0530

    Explore Design Structure

--- client/src/pages/Explore/Explore.jsx
+++ client/src/pages/Explore/Explore.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import './Explore.css'
 
 const Explore = () => {
   return (
-    <div>Explore</div>
-  )
-}
+    <div className="explore-container text-light">
+      <div className="left-column">
+        <div className="first-row" style={{overflowY: 'auto'}}>
+          Categories
+        </div>
+        <hr className="horizontal-line" />
+        <div className="second-row" style={{overflowY: 'auto'}}>
+          Items
+        </div>
+      </div>
+      <div className="right-column d-flex flex-column">
+        <div className="customer-form-container" style={{height: '15%'}}>
+          Customer Form
+        </div>
+        <hr className="my-3 text-light" />
+        <div className="cart-items-container" style={{height: '55%', overflowY: 'auto'}}>
+          Cart Items
+        </div>
+        <div className="cart-summary-container" style={{height: '30%'}}>
+          Cart Summary
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Explore
+export default Explore;
