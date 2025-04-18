897fd83d0d5ad8571ac6c923e99e3a43d526801a
YashwantXDX <yashwant.choure73546@gmail.com>
2025-04-18 15:12:12 +0530

    Category Searching Done

--- client/src/components/CategoryList/CategoryList.jsx
+++ client/src/components/CategoryList/CategoryList.jsx
@@ -1,18 +1,37 @@
 import React, { useContext } from "react";
 import { AppContext } from "../../context/AppContext.jsx";
 import "./CategoryList.css";
+import { useState } from "react";
 
 const CategoryList = () => {
   const { categories } = useContext(AppContext);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filterCategories = categories.filter(category => 
+    category.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div
       className="category-list-container"
       style={{ height: "100vh", overflowY: "auto", overflowX: "hidden" }}
     >
-      <div className="row pe-2">Search Bar</div>
+      <div className="row pe-2">
+        <div className="input-group mb-3">
+          <input type="text" 
+          name="keyword" 
+          placeholder="Search By Keyword" 
+          className="form-control"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
+          />
+          <span className="input-group-text bg-warning">
+            <i className="bi bi-search"></i>
+          </span>
+        </div>
+      </div>
       <div className="row g-3 pe-2">
-        {categories.map((category, index) => (
+        {filterCategories.map((category, index) => (
           <div key={index} className="col-12">
             <div
               className="card p-3"
@@ -28,7 +47,7 @@ const CategoryList = () => {
                 </div>
                 <div className="flex-grow-1">
                   <h5 className="mb-1 text-white">{category.name}</h5>
-                  <p className="mb-0 text-white">{category.items} Items</p>
+                  <p className="mb-0 text-white">5 Items</p>
                 </div>
                 <div>
                   <button className="btn btn-danger btn-sm">
