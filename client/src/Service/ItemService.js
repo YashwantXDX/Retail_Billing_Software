a08baa5f49092c05a518b3d5b81fc92e4ea38076
YashwantXDX <yashwant.choure73546@gmail.com>
2025-04-19 19:46:01 +0530

    Items Uploading Implemented

--- client/src/Service/ItemService.js
+++ client/src/Service/ItemService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const addItem = async (item) => {
-    return await axios.post("http://localhost:8080/api/v1.0/admin/items", item, {headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}});
+    return await axios.post(`http://localhost:8080/api/v1.0/admin/items`, item, {headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}});
 }
 
 export const deleteItem = async (itemId) => {
--- client/src/components/ItemForm/ItemForm.jsx
+++ client/src/components/ItemForm/ItemForm.jsx
@@ -1,68 +1,166 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import {assets} from "../../assets/assets.js";
+import {AppContext} from "../../context/AppContext.jsx";
+import toast from 'react-hot-toast';
+import {addItem} from '../../Service/ItemService.js'
 
 const ItemForm = () => {
-  return (
-    <div className="item-form-container" style={{height: '100vh', overflowY: 'auto', overflowX: 'hidden'}}>
-        <div className="mx-2 mt-2">
-            <div className="row">
-                <div className="card col-md-8 form-container">
-                    <div className="card-body">
-                        <form>
-                            <div className="mb-3">
-                                <label htmlFor="image" className='form-label'>
-                                    <img src="https://placehold.co/48x48" alt="" width={48}/>
-                                </label>
-                                <input type="file" name="image" id="image" className='form-control' hidden />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="name" className='form-label'>Name</label>
-                                <input type="text" 
-                                    name='name' 
-                                    id='name'
-                                    className='form-control'
-                                    placeholder='Item Name'
-                                />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="catgory" 
-                                    className='form-label'
-                                >Category</label>
-                                <select name="category" id="category" className="form-control">
-                                    <option value="">--SELECT CATEGORY--</option>
-                                    <option value="">Category 1</option>
-                                    <option value="">Category 2</option>
-                                    <option value="">Category 3</option>
-                                </select>
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="price" className="form-label">Price</label>
-                                <input type="number" 
-                                    name="price" 
-                                    id="price" 
-                                    className='form-control'
-                                    placeholder='&#8377; 200.00'
-                                    />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="description" className='form-label'>Description</label>
-                                <textarea
-                                    rows="5"
-                                    name='description' 
-                                    id='description'
-                                    className='form-control'
-                                    placeholder='Item description'
-                                />
-                            </div>
-                            <button type='submit'
-                                className='btn btn-warning w-100'
-                            >Save</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+    
+    const {categories, setItemsData, itemsData} = useContext(AppContext);
+    const [image, setImage] = useState(false);
+    const [data, setData] = useState({
+        name: "",
+        categoryId: "",
+        price: "",
+        description: "",
+    });
+    const [loading, setLoading] = useState(false);
 
-export default ItemForm
+    const onChangeHandler = (e) => {
+        const value = e.target.value;
+        const name = e.target.name;
+
+        setData((data) => (
+            {
+                ...data, [name]: value
+            }
+        ));
+    }
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        const formData = new FormData();
+        formData.append("item", JSON.stringify(data));
+        formData.append("file", image);
+        
+        try{
+            if(!image){
+                toast.error("Select Image");
+                return;
+            }    
+
+            const response = await addItem(formData);
+            if(response.status === 201){
+                setItemsData([...itemsData, response.data]);
+                
+                //TODO: Update the category state
+
+                toast.success("Item Added");
+                setData({
+                    name: "",
+                    description: "",
+                    price: "",
+                    categoryId: "",
+                });
+                setImage(false);
+            }
+            else{
+                toast.error("Unable to add item");
+            }
+        }
+        catch(e){
+            console.log(e);
+            toast.error("Unable to add item");
+        }
+        finally{
+            setLoading(false);
+        }
+    }
+  
+    return (
+    <div
+      className="item-form-container"
+      style={{ height: "100vh", overflowY: "auto", overflowX: "hidden" }}
+    >
+      <div className="mx-2 mt-2">
+        <div className="row">
+          <div className="card col-md-8 form-container">
+            <div className="card-body">
+              <form onSubmit={onSubmitHandler}>
+                <div className="mb-3">
+                  <label htmlFor="image" className="form-label">
+                    <img src={image ? URL.createObjectURL(image) : assets.upload} alt="" width={48} />
+                  </label>
+                  <input
+                    type="file"
+                    name="image"
+                    id="image"
+                    className="form-control"
+                    hidden
+                    onChange={(e) => setImage(e.target.files[0])}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="name" className="form-label">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="form-control"
+                    placeholder="Item Name"
+                    onChange={onChangeHandler}
+                    value={data.name}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="catgory" className="form-label">
+                    Category
+                  </label>
+                  <select
+                    name="categoryId"
+                    id="category"
+                    className="form-control"
+                    onChange={onChangeHandler}
+                    value={data.categoryId}
+                  >
+                    <option value="">--SELECT CATEGORY--</option>
+                    {categories.map((category, index) => (
+                        <option key={index} value={category.categoryId}>{category.name}</option>
+                    ))}
+                  </select>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="price" className="form-label">
+                    Price
+                  </label>
+                  <input
+                    type="number"
+                    name="price"
+                    id="price"
+                    className="form-control"
+                    placeholder="&#8377; 200.00"
+                    onChange={onChangeHandler}
+                    value={data.price}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="description" className="form-label">
+                    Description
+                  </label>
+                  <textarea
+                    rows="5"
+                    name="description"
+                    id="description"
+                    className="form-control"
+                    placeholder="Item description"
+                    onChange={onChangeHandler}
+                    value={data.description}
+                  />
+                </div>
+                <button type="submit" className="btn btn-warning w-100" disabled={loading}>
+                  {loading ? "Loading" : "Save"}
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ItemForm;
