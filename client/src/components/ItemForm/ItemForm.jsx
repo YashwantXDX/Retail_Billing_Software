ae9a49fc96c6fecc7928359038e5f49d64ef34bc
YashwantXDX <yashwant.choure73546@gmail.com>
2025-04-20 12:38:16 +0530

    Items Addition, Deletion Working

--- client/src/components/ItemForm/ItemForm.jsx
+++ client/src/components/ItemForm/ItemForm.jsx
@@ -6,7 +6,7 @@ import {addItem} from '../../Service/ItemService.js'
 
 const ItemForm = () => {
     
-    const {categories, setItemsData, itemsData} = useContext(AppContext);
+    const {categories, setItemsData, itemsData, setCategories} = useContext(AppContext);
     const [image, setImage] = useState(false);
     const [data, setData] = useState({
         name: "",
@@ -44,8 +44,9 @@ const ItemForm = () => {
             const response = await addItem(formData);
             if(response.status === 201){
                 setItemsData([...itemsData, response.data]);
-                
-                //TODO: Update the category state
+                setCategories((prevCategories) =>
+                  prevCategories.map((category) => category.categoryId === data.categoryId ? {...category, items: category.items + 1} : category)
+                )
 
                 toast.success("Item Added");
                 setData({
