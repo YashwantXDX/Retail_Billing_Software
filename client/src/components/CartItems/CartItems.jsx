72967e70f2679196d29e5d1d018e86be90a94ac5
YashwantXDX <yashwant.choure73546@gmail.com>
2025-04-20 15:01:41 +0530

    Cart Items Showing

--- client/src/components/CartItems/CartItems.jsx
+++ client/src/components/CartItems/CartItems.jsx
@@ -5,11 +5,45 @@ import { AppContext } from '../../context/AppContext';
 const CartItems = () => {
 
   const {cartItems} = useContext(AppContext);
-  console.log("From cart Items component", cartItems);
-  
 
   return (
-    <div>CartItems</div>
+    <div className="p-3 h-100 overflow-y-auto">
+        {cartItems.length === 0 ? (
+          <p className="text-light">
+            Your Cart is Empty
+          </p>
+        ) : (
+          <div className="cart-items-list">
+            {cartItems.map((item, index) => (
+              <div key={index} className="cart-item mb-3 p-3 bg-dark rounded">
+                <div className="d-flex justify-content-between align-items-center mb2">
+                  <h6 className="mb-0 text-light">{item.name}</h6>
+                  <p className="mb-0 text-light">
+                  &#8377; {(item.price * item.quantity).toFixed(2)}
+                  </p>
+                </div>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex align-items-center gap-2">
+                    <button className="btn btn-danger btn-sm" disabled={item.quantity === 1}>
+                      <i className="bi bi-dash"></i>
+                    </button>
+                    <span className="text-light">{item.quantity}</span>
+                    <button className="btn btn-primary btn-sm">
+                      <i className="bi bi-plus"></i>
+                    </button>
+                  </div>
+                  <button 
+                  className="btn btn-danger btn-sm"
+                  style={{width: 'auto'}}
+                  >
+                    <i className="bi bi-trash"></i>
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+    </div>
   )
 }
 
