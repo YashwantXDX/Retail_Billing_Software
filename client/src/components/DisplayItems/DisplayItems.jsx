06f9d597e677a340066a257c9c578da4849f800d
YashwantXDX <yashwant.choure73546@gmail.com>
2025-04-20 14:08:27 +0530

    Display Items Implimented

--- client/src/components/DisplayItems/DisplayItems.jsx
+++ client/src/components/DisplayItems/DisplayItems.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './DisplayItems.css';
+import {AppContext} from '../../context/AppContext.jsx'
+import Item from '../Item/Item.jsx';
 
 const DisplayItems = () => {
+
+  const {itemsData} = useContext(AppContext);
+
   return (
-    <div>DisplayItems</div>
+    <div className="p-3">
+      <div className="row g-3">
+        {itemsData.map((item, index) => (
+          <div key={index} className='col-md-4 col-sm-6'>
+            <Item 
+              itemName={item.name}
+              itemPrice={item.price}
+              itemImage={item.imgUrl}
+              itemId={item.itemId}
+            ></Item>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
