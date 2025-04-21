fc62fa54578c3be23009c12e7d41116f39d099b0
YashwantXDX <yashwant.choure73546@gmail.com>
2025-04-21 22:25:16 +0530

    Route Protection Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Menubar from './components/Menubar/Menubar.jsx'
-import {Route, Routes, useLocation} from 'react-router-dom';
+import {Navigate, Route, Routes, useLocation} from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard.jsx'
 import ManageCategory from './pages/ManageCategory/ManageCategory.jsx'
 import ManageUsers from './pages/ManageUsers/ManageUsers.jsx'
@@ -9,10 +9,31 @@ import Explore from './pages/Explore/Explore.jsx'
 import {Toaster} from 'react-hot-toast'
 import Login from './pages/Login/Login.jsx';
 import OrderHistory from './pages/OrderHistory/OrderHistory.jsx';
+import { AppContext } from './context/AppContext.jsx';
 
 const App = () => {
 
   const location = useLocation();
+  const {auth} = useContext(AppContext);
+
+  const LoginRoute = ({element}) => {
+    if(auth.token){
+      return <Navigate to='/dashboard' replace />;
+    }
+    return element;
+  }
+
+  const ProtectedRoute = ({element, allowedRoles}) => {
+    if(!auth.token){
+      return <Navigate to='/login' replace/>;
+    }
+
+    if(allowedRoles && !allowedRoles.includes(auth.role)){
+      return <Navigate to='/dashboard' replace/>
+    }
+
+    return element;
+  }
 
   return (
     <div>
@@ -20,13 +41,16 @@ const App = () => {
       <Toaster />
       <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/category" element={<ManageCategory />} />
-          <Route path="/users" element={<ManageUsers />} />
-          <Route path="/items" element={<ManageItems />} />
           <Route path="/explore" element={<Explore />} />
-          <Route path="/login" element={<Login />} />
+          {/* Admin only routes */}
+          <Route path="/category" element={<ProtectedRoute element={<ManageCategory />} allowedRoles={['ROLE_ADMIN']}/>} />
+          <Route path="/users" element={<ProtectedRoute element={<ManageUsers />} allowedRoles={['ROLE_ADMIN']}/>} />
+          <Route path="/items" element={<ProtectedRoute element={<ManageItems />} allowedRoles={['ROLE_ADMIN']}/>} />
+
+          <Route path="/login" element={<LoginRoute element={<Login /> }/>} />
           <Route path="/orders" element={<OrderHistory />} />
           <Route path="/" element={<Dashboard />} />
+          
       </Routes>
     </div>
   )
